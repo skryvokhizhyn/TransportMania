96cd022adef817dc2c3773cb40629de67359f270
Sergii K. <skryvokhizhyn@gmail.com>
2014-04-08 22:49:39 +0300

    test 3

--- jni/hello-jni.c
+++ jni/hello-jni.c
@@ -27,5 +27,5 @@ jstring
 Java_com_chilloutsoft_transportmania_TransportManiaActivity_stringFromJNI( JNIEnv* env,
                                                   jobject thiz )
 {
-    return (*env)->NewStringUTF(env, "Hello from JNI TROLOLO 2 !");
+    return (*env)->NewStringUTF(env, "Hello from JNI TROLOLO 3 !");
 }
