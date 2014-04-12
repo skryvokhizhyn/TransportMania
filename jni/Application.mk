fd5d4c10f761ba2a5d118fbdf5d51b4383ad4a70
Sergii K. <skryvokhizhyn@gmail.com>
2014-04-12 21:10:02 +0300

    added exceptionsand rtti

--- jni/Application.mk
+++ jni/Application.mk
@@ -4,8 +4,18 @@
 # otherwise, the following line select the latest clang version.
 # NDK_TOOLCHAIN_VERSION := clang
 # enable c++11 extensions in source code
-APP_CPPFLAGS += -std=c++11
+APP_CPPFLAGS += -std=c++11 -fexceptions -frtti
  or use APP_CPPFLAGS := -std=gnu++11
 # Following is STL implementation. Can be use _static or _shared version, the last one need a call to loadLibrary BEFORE use the C++ code in the shared library
 #APP_STL := gnustl_static
 APP_STL := stlport_static
+
+#APP_ABI := armeabi armeabi-v7a x86
+ifneq (,$(findstring windows, $(OS)))
+  APP_ABI := x86
+else
+  APP_ABI := armeabi-v7a
+endif
+
+
+
--- jni/TransportManiaNative.cpp
+++ jni/TransportManiaNative.cpp
@@ -1,8 +1,32 @@
 #include <jni.h>
 #include <string>
+#include <stdexcept>
+
+struct Base
+{
+	virtual ~Base() {}
+};
+
+struct Derived : Base
+{
+};
+
 extern "C" {
 const char * test()
 {
+	try
+	{
+		throw std::runtime_error("exc");
+	}
+	catch (const std::exception & e)
+	{
+		(e);
+	}
+	
+	Base * b = new Derived();
+	Derived * d = dynamic_cast<Derived*>(b);
+	delete d;
+	
 	static std::string a = "asdfs";
 	auto asd = [](){return "sdfhj";};
 	a = asd();
