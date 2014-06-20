8124ec39d2fcdf266b1f5880b0022bdb9c5498ca
Sergii K <vv1ld@ua.fm>
2014-06-20 14:22:17 +0300

    project structure change # 1
    moved android project to android subfolder

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -5,17 +5,17 @@
 # NDK_TOOLCHAIN_VERSION := clang
 # enable c++11 extensions in source code
 APP_CPPFLAGS += -std=c++11 -fexceptions -frtti
- or use APP_CPPFLAGS := -std=gnu++11
+# or use APP_CPPFLAGS := -std=gnu++11
 # Following is STL implementation. Can be use _static or _shared version, the last one need a call to loadLibrary BEFORE use the C++ code in the shared library
 #APP_STL := gnustl_static
 APP_STL := stlport_static
 
 #APP_ABI := armeabi armeabi-v7a x86
-ifneq (,$(findstring windows, $(OS)))
-  APP_ABI := x86
-else
+#ifneq (,$(findstring Windows, $(OS)))
+#  APP_ABI := x86
+#else
   APP_ABI := armeabi-v7a
-endif
+#endif
 
 
 
