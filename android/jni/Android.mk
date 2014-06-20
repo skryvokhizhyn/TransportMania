3e3e72c8d14fee7eb61f73a4f00aee30f2c8a5f7
Sergii K <vv1ld@ua.fm>
2014-06-21 00:23:36 +0300

    SDL run on emulator

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -12,12 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE    := libSDL2 
+LOCAL_SRC_FILES := ../../3rdparty/sdl2/lib/$(TARGET_ARCH_ABI)/libSDL2.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE    := TransportManiaNative
-LOCAL_SRC_FILES := hello-jni.c TransportManiaNative.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../3rdparty/SDL2/include
+
+#LOCAL_SRC_FILES := hello-jni.c TransportManiaNative.cpp
+LOCAL_SRC_FILES := SDL_android_main.c TransportManiaNative.cpp
+
+LOCAL_LDLIBS := -ldl -lGLESv1_CM -lGLESv2 -llog -landroid 
+#LOCAL_STATIC_LIBRARIES := -l../../3rdparty/SDL2/lib/armeabi-v7a/libSDL2.a
+LOCAL_STATIC_LIBRARIES := libSDL2
 
 include $(BUILD_SHARED_LIBRARY)
 
