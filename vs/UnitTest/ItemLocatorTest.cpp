73404976d66c619272dc9a4bd9e8ca8e80ed6e6e
Sergii K <vv1ld@ua.fm>
2015-01-17 23:07:35 +0200

    Fixes in tests

--- vs/UnitTest/ItemLocatorTest.cpp
+++ vs/UnitTest/ItemLocatorTest.cpp
@@ -8,7 +8,8 @@ using namespace trm;
 BOOST_AUTO_TEST_CASE(ItemLocatorTest1)
 {
 	ItemLocator il;
-	il.Put(0, {Point2d(0, 0), Point2d(0, 1), Point2d(1, 1), Point2d(1, 0)});
+	UniqueId id;
+	il.Put(id, {Point2d(0, 0), Point2d(0, 1), Point2d(1, 1), Point2d(1, 0)});
 
 	BOOST_CHECK_EQUAL(il.At({Point2d(0, 0), Point2d(0, 1), Point2d(1, 1)}).size(), 1u);
 	BOOST_CHECK_EQUAL(il.At({Point2d(0.3f, 0.3f), Point2d(0.3f, 0.6f), Point2d(0.6f, 0.6f)}).size(), 1u);
@@ -18,7 +19,7 @@ BOOST_AUTO_TEST_CASE(ItemLocatorTest1)
 
 	BOOST_CHECK_EQUAL(il.At({Point2d(-0.3f, -0.3f), Point2d(-0.3f, -0.6f), Point2d(-0.6f, -0.6f)}).size(), 0u);
 
-	il.Remove(0);
+	il.Remove(id);
 
 	BOOST_CHECK_EQUAL(il.At({Point2d(0, 0), Point2d(0, 1), Point2d(1, 1)}).size(), 0u);
 }
@@ -26,30 +27,32 @@ BOOST_AUTO_TEST_CASE(ItemLocatorTest1)
 BOOST_AUTO_TEST_CASE(ItemLocatorTest2)
 {
 	ItemLocator il;
-	il.Put(0, {Point2d(0, 0), Point2d(0, 1), Point2d(1, 1), Point2d(1, 0)});
+	UniqueId id;
+	il.Put(id, {Point2d(0, 0), Point2d(0, 1), Point2d(1, 1), Point2d(1, 0)});
 
-	BOOST_CHECK_THROW(il.Put(0, {Point2d(0, 0)}), std::runtime_error);
-	BOOST_CHECK_THROW(il.Remove(1), std::runtime_error);
+	BOOST_CHECK_THROW(il.Put(id, {Point2d(0, 0)}), std::runtime_error);
+	BOOST_CHECK_THROW(il.Remove(UniqueId()), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(ItemLocatorTest3)
 {
 	ItemLocator il;
-	il.Put(0, {Point2d(-1, -1), Point2d(-1, 1), Point2d(0, 1), Point2d(0, -1)});
-	il.Put(1, {Point2d(1, 1), Point2d(1, -1), Point2d(0, -1), Point2d(0, 1)});
+	UniqueId id1, id2;
+	il.Put(id1, {Point2d(-1, -1), Point2d(-1, 1), Point2d(0, 1), Point2d(0, -1)});
+	il.Put(id2, {Point2d(1, 1), Point2d(1, -1), Point2d(0, -1), Point2d(0, 1)});
 	
 	auto ids = il.At({Point2d(-0.5, -0.5), Point2d(-0.5, 0.5), Point2d(0.5, 0.5)});
 
 	// order not guaranteed
-	BOOST_CHECK_EQUAL(ids[0], 0);
-	BOOST_CHECK_EQUAL(ids[1], 1);
+	BOOST_CHECK_EQUAL(ids[0], id1);
+	BOOST_CHECK_EQUAL(ids[1], id2);
 }
 
 BOOST_AUTO_TEST_CASE(ItemLocatorTest4)
 {
 	ItemLocator il;
-	il.Put(0, {Point2d(-2, -1), Point2d(-2, 1), Point2d(-1, 1), Point2d(-1, -1)});
-	il.Put(1, {Point2d(2, 1), Point2d(2, -1), Point2d(1, -1), Point2d(1, 1)});
+	il.Put(UniqueId(), {Point2d(-2, -1), Point2d(-2, 1), Point2d(-1, 1), Point2d(-1, -1)});
+	il.Put(UniqueId(), {Point2d(2, 1), Point2d(2, -1), Point2d(1, -1), Point2d(1, 1)});
 	
 	BOOST_CHECK_EQUAL(il.At({Point2d(-0.5, -0.5), Point2d(-0.5, 0.5), Point2d(0.5, 0.5)}).size(), 0u);
 }
